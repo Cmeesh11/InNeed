1c3bf3ac89d67840b4fbb9b6bb9fcb4a7e91a212
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-10 19:52:25 -0400

    modified boilerplate

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 const jobSeekerRouter = require('./jobSeeker');
-const employer = require('./employer');
+const employerRouter = require('./employer');
 
 router.use('/jobSeeker', jobSeekerRouter);
 router.use('/employer', employerRouter);
