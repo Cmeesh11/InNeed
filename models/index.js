30947f5837e15e58f74a6d076965aef986eb4fd1
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-12 19:48:16 -0400

    created models

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Post = require('./Post.js');
+const Employer = require('./Employer.js');
+
+
+Employer.hasMany(Post, {
+  foreignKey: 'employer_id'
+})
+
+Post.belongsTo(Employer, {
+  foreignKey: 'employer_id'
+})
+
+module.exports = { Employer, Post }
