8f0fe691e6bc434428dcf8d759a97c3c55b7797f
Nate Long <nate.k.long@gmail.com>
2023-04-13 19:06:16 -0400

    commiting

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Employer, JobSeeker, Post } = require('../models')
+const { Employer, JobSeeker, Post } = require('../models');
 const jobSeekerRouter = require('./jobSeeker');
 const employerRouter = require('./employer');
 
@@ -9,17 +9,16 @@ router.use('/employer', employerRouter);
 // Routes go here
 
 router.get('/', async function(req, res) {
-    try {
+  try {
     const posts = await Post.findAll();
     res.render('homepage', {
-      loggedIn: req.session.loggedIn
+      loggedIn: req.session.loggedIn,
       posts
-    })
+    });
   } catch (err) {
     res.status(500).json(err);
-    }
+  }
 });
-
 
 router.get('/login', async (req, res) => {
   try {
