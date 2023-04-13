cc4948f862f4c1082cba3c2fa15b8885885d404c
Jacob Milojkovich <jacobmilojkovich@jacobs-mbp.mynetworksettings.com>
2023-04-12 20:06:30 -0400

    Added homepage route

--- controllers/index.js
+++ controllers/index.js
@@ -9,4 +9,12 @@ router.use('/employer', employerRouter);
 // Routes go here
 //route.get()
 
+router.get('/', function(req, res) {
+    try {
+    res.render('homepage', {loggedIn: req.session.loggedIn})
+} catch (err) {
+    res.status(500).json(err);
+  }})
+  
+
 module.exports = router;
