26e01b8676164af4c9c59d72946636534d43fb36
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-12 21:27:01 -0400

    started jobpost.handlebars file

--- controllers/employer.js
+++ controllers/employer.js
@@ -1,5 +1,11 @@
 const employerRouter = require('express').Router();
 
-employerRouter.get('/post')
+employerRouter.get('/post', async (req, res) => {
+  try {
+    res.render('jobpost');
+  } catch (err) {
+    res.status(500).json(err);
+  }
+})
 
 module.exports = employerRouter;
