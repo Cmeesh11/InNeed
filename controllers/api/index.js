c5a72feba295869fdc7e4569b3437b19fd65d84f
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-17 19:15:12 -0400

    created api/userRoutes files

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const postRouter = require('./postRoutes');
+const userRouter = require('./userRoutes');
 
 router.use('/post', postRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
