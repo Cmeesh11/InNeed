6728783159ddad9c87ce2a01e2ca78bc40a6cfaf
Cmeesh11 <cartermeesh11@gmail.com>
2023-04-17 19:26:57 -0400

    changed routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,31 @@
 const userRouter = require('express').Router();
+const JobSeeker = require('../../models/JobSeeker');
+const { Employer } = require('../../models');
 
-userRouter.post('/employerSignup', async (req, res) => {
+// Router: Create Employer Signup
+userRouter.post('/signupEmployer', async (req, res) => {
+  try {
+    const employer = await Employer.create(req.body);
+    req.session.logged_in = true;
+    req.session.employer = true;
+    req.session.id = employer.id;
+    req.session.save();
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+// Router: Create Job Seeker Signup
+userRouter.post('/signupJobseeker', async (req, res) => {
+  try {
+    const jobSeeker = await JobSeeker.create(req.body);
+    req.session.logged_in = true;
+    req.session.JobSeeker = true;
+    req.session.id = jobSeeker.id;
+    req.session.save();
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = userRouter;
--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Employer, JobSeeker, Post } = require('../models');
+const { Employer, Post } = require('../models');
 const jobSeekerRouter = require('./jobSeekerRoutes');
 const employerRouter = require('./employerRoutes');
 const apiRouter = require('./api/index');
@@ -36,28 +36,6 @@ router.get('/logout', (req, res) => {
     res.render('dashboard', {
       logged_in: req.session.logged_in
     });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Router: Create Employer Signup
-router.post('/signupEmployer', async (req, res) => {
-  try {
-    const employer = await Employer.create(req.body);
-    req.session.logged_in = true;
-    req.session.id = employer.id;
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// Router: Create Job Seeker Signup
-router.post('/signupJobseeker', async (req, res) => {
-  try {
-    const jobSeeker = await JobSeeker.create(req.body);
-    req.session.logged_in = true;
-    req.session.id = jobSeeker.id;
   } catch (err) {
     res.status(500).json(err);
   }
