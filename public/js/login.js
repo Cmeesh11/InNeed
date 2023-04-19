0e7954316a25af3b29bccbf86b36aac524c97ec2
Nate Long <nate.k.long@gmail.com>
2023-04-19 19:14:48 -0400

    no clue what im commiting

--- public/js/login.js
+++ public/js/login.js
@@ -15,3 +15,28 @@ jobseekerCheckbox.on('click', () => {
 });
 
 companyInput.hide();
+
+// When form is submitted, confirmation page is rendered
+$('#post-form').on('submit', async (event) => {
+  event.preventDefault();
+  // Selecting elements
+  const jobTitle = $('.job-title').val().trim();
+  const salary = $('.salary').val().trim();
+  const state = $('#dropdown-menu').val();
+  const requirements = $('.requirements').val().trim();
+  const description = $('.job-description').val().trim();
+
+  try {
+    const response = await fetch('/api/post/create', {
+      method: 'POST',
+      body: JSON.stringify({ job_title: jobTitle, salary, state, requirements, description }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+    if (response.ok) {
+      document.location.replace('/employer/confirm');
+    }
+  } catch (err) {
+    alert('Post was not created. Please try again');
+    document.location.replace('/employer/post');
+  }
+});
